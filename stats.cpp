41d3b23cdb1e37d659ae17ca9784fab93b99c3ea
Rishon_Dsouza <rishonv.dsouza@gmail.com>
2020-10-05 13:29:23 +0400

    inital commit

---
+++ stats.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+  int x,y=0;
+  float av=0;
+  cout << "Enter 2 numbers"<< endl;
+  cin >> x>>y;
+  cout <<"your numbers are " <<x <<" and " <<y <<endl;
+  cout <<"Sum: " <<x+y <<endl;
+  cout <<"Difference: " <<x-y <<endl;
+  cout <<"Product: " <<x*y <<endl;
+  cout <<"Distance: "<<abs(x-y)<<endl;
+  av=(x+y)/2.0;
+  cout <<"Average: "<<av;
+}
